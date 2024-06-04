2d3dc30519f3e3d2989004c4113f74d79520f12b
Sanjula Maneth <manethsanjula@gmail.com>
2024-06-04 16:04:49 +0530

    completed

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect
 from flask_cors import CORS
 import requests
 app = Flask(__name__)
 CORS(app)
 
+#Starting page
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('form.html', value_entered=None)
 
 
+# Normal Method of post (internal method)
+@app.route('/demo', methods=['POST'])
+def demo():
+    if request.method == 'POST':
+        data = request.form.get('staticEmail2')
+        print(data)
+        return render_template("form.html", value_entered=data)
+        #return jsonify({'data': data})
+
+
+
+#API Call And response function
 @app.route('/process', methods=['POST'])
 def process():
     data = request.form.get('data')
